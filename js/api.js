5a4e777288f9c886d8681a9012160ce22edc328d
zun3017 <hodung300107@gmail.com>
2026-07-15 07:24:20 +0700

    Cap nhat SCRIPT_URL moi nhat trong js/api.js sau khi sua loi setSharing

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
         // URL Web App của Google Apps Script
-        const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwH0qPLN3MPpjIx42vdYYQPjGtIlw_oYu8J4lrZ6IxcHF3CCShgiZIp-tLdkWYTkdLe/exec';
+        const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyirp8Sw_3NtWdfRfEyBsNTyGXP7XA5diycRd2chmes5I0JjEo2FabVzmwdcSJR5Mhx/exec';
 
         // Chỉ tạo Shim giả lập nếu chạy ngoài môi trường Google Apps Script (ví dụ trên GitHub Pages)
         if (typeof google === 'undefined' || typeof google.script === 'undefined' || typeof google.script.run === 'undefined') {
