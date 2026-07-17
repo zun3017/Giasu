324d5828c71e364e9a3d0cb6446a888766a24d65
zun3017 <hodung300107@gmail.com>
2026-07-18 00:22:16 +0700

    config: update Google Apps Script SCRIPT_URL to AKfycbx4ahsAItR8F-x-IdNGFtKS0prc67YbfaQY0yMG2qLQ7-grcLFHwwXJUN4JtcwbqZrF and bump cache to v=1.6.7

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-        const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxpf6hUf3HexiHLZ4hKS-sEX7pU9Jz99GdZg9w6ONPJt8-lpKanc3h-6bwpLqaWffEe/exec';
+        const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx4ahsAItR8F-x-IdNGFtKS0prc67YbfaQY0yMG2qLQ7-grcLFHwwXJUN4JtcwbqZrF/exec';
 
         // Chỉ tạo Shim giả lập nếu chạy ngoài môi trường Google Apps Script (ví dụ trên GitHub Pages)
         if (typeof google === 'undefined' || typeof google.script === 'undefined' || typeof google.script.run === 'undefined') {
