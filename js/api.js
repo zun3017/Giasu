a75fff046d4f3ea9098ca1bb38022232305f30aa
zun3017 <hodung300107@gmail.com>
2026-07-23 13:13:51 +0700

    Cập nhật API URL để sửa lỗi giáo viên không nhận được ý kiến

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-        const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxEDv-7ZNmBkq_1GtnGHjMVgqIHdAIH8-gNfqwXeBwlJaUMgc3buRB3bK7EveTEs_0K/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyiJzC7dV6Bq5-E7O7U2pAfg68vFcgI0pdAuEP2UxXgwfuRYiiVFddbaiQK8_dlFPmu/exec';
 
         // Chỉ tạo Shim giả lập nếu chạy ngoài môi trường Google Apps Script (ví dụ trên GitHub Pages)
         if (typeof google === 'undefined' || typeof google.script === 'undefined' || typeof google.script.run === 'undefined') {
